ac5fed192c41c69b50bd2d4e11a216f0a0c0fa34
Steven Nguyen <72920999+PopcraftReal@users.noreply.github.com>
2025-07-20 15:09:41 +0000

    setup bot

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import discord
 from dotenv import load_dotenv
 import os
+from keep_alive import keep_alive
 
 PREFIX = '-'
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+if (TOKEN is None):
+    raise Exception('No token found!')
+
 
 class Client(discord.Client):
+
     async def on_ready(self):
         print(f'Hello, I\'m ready! {self.user}')
-    
-    async def on_message(self, msg: discord.Message):
-        print(f"Message from {msg.author}: {msg.content}")
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
 client = Client(intents=intents)
+keep_alive()
 client.run(TOKEN)
