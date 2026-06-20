139c096af17c3af851103e4dc95390a24c0bdcdf
Poppy <ndvu2004@gmail.com>
2026-06-21 01:20:27 +1000

    add cocktower cog into the bot

--- src/BotClient.py
+++ src/BotClient.py
@@ -1,9 +1,13 @@
 from discord.ext import commands
 
+from src.cogs import CocktowerCog
+
 class BotClient(commands.Bot):
     async def on_ready(self):
+        print("Add cogs...")
+        await self.add_cog(CocktowerCog())
+        
         print(f'Hello, I\'m ready! {self.user}')
-
         try:
             synced = await self.tree.sync()
             print(f"Synced {len(synced)} command(s)")
